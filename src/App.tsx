ee546d5bf45f5bd4895ad065b7243f1c28d4c14f
Yaswanth <71338071+yash-phoenix79@users.noreply.github.com>
2024-11-12 23:45:07 +0530

    implemented extra reducers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import store from "./store/store"
 import { Container, Typography } from "@mui/material"
 import AddHabitForm from "./components/add-habit-form"
 import HabitList from "./components/habit-list"
+import HabitStats from "./components/habit-stats"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Typography component="h1" variant="h2" align="center">Habit Tracker</Typography>
           <AddHabitForm />
           <HabitList />
+          <HabitStats />
         </Container>
       </Provider>
     </>
